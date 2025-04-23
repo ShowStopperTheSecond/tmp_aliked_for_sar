e71eeea7cb64f391f9f38449ccc2342a91540194
javid <norouzi.javid@protonmail.com>
2025-04-23 23:39:34 +0330

    bug fixed

--- nets/sampler.py
+++ nets/sampler.py
@@ -479,13 +479,14 @@ class NghSampler3 (nn.Module):
         # compute positive scores
         # xy2p = clamp(xy2[:,None,:] + self.pos_offsets[:,:,None])
         print(f'mask: {mask.shape}, xy2: {xy2.shape}')
+        print(mask[:100])
         xy2p = xy2
         feat2 = feat2[b2, :, xy2p[1], xy2p[0]]
 
 
         feat1 = feat1[mask]
         feat2 = feat2[mask]
-        
+
 
         print(torch.any(torch.isnan(feat1)))
         print(torch.any(torch.isnan(feat2)))
