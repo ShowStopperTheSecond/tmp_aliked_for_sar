e53db2fbd24443a3eeb470728d5cd1c2e9c60ef7
javid <norouzi.javid@protonmail.com>
2025-04-23 21:29:41 +0330

    bug fixed

--- nets/aliked.py
+++ nets/aliked.py
@@ -183,7 +183,7 @@ class ALIKED(nn.Module):
         # t1 = time.time()        
 
         
-        return self.normalize(feature_map, score_map, None)
+        return self.normalize(feature_map, None, score_map)
         # return {'keypoints': keypoints,  # B N 2
         #     'descriptors': descriptors,  # B N D
         #     'reliability': reliability,
